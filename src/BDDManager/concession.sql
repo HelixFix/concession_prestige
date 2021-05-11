f60810184baada7ae3e2aa60a688ef98e9080e6e
MIk08 <mickael.henry.mh@gmail.com>
2021-05-11 14:56:42 +0200

    nouvelle base de donnée

--- src/BDDManager/concession.sql
+++ src/BDDManager/concession.sql
@@ -1,47 +1,36 @@
-<<<<<<< HEAD
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+DROP DATABASE IF EXISTS CONCESSION;
+
+CREATE DATABASE CONCESSION;
+
+USE CONCESSION;
 
 
-#------------------------------------------------------------
-# Table: client
-#------------------------------------------------------------
 
 CREATE TABLE client(
         id_client Int  Auto_increment  NOT NULL ,
         nom       Varchar (50) NOT NULL ,
         prenom    Varchar (50) NOT NULL ,
         telephone Varchar (50) NOT NULL
-	,CONSTRAINT client_PK PRIMARY KEY (id_client)
+    ,CONSTRAINT client_PK PRIMARY KEY (id_client)
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: model
-#------------------------------------------------------------
 
 CREATE TABLE model(
         id_modele     Int  Auto_increment  NOT NULL ,
         libelle_model Varchar (40) NOT NULL
-	,CONSTRAINT model_PK PRIMARY KEY (id_modele)
+    ,CONSTRAINT model_PK PRIMARY KEY (id_modele)
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: constructeur
-#------------------------------------------------------------
 
 CREATE TABLE constructeur(
         id_constructeur      Int  Auto_increment  NOT NULL ,
         libelle_constructeur Varchar (40) NOT NULL
-	,CONSTRAINT constructeur_PK PRIMARY KEY (id_constructeur)
+    ,CONSTRAINT constructeur_PK PRIMARY KEY (id_constructeur)
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: voiture
-#------------------------------------------------------------
 
 CREATE TABLE voiture(
         id_voiture      Int  Auto_increment  NOT NULL ,
@@ -51,13 +40,14 @@ CREATE TABLE voiture(
         nombre_de_porte Int NOT NULL ,
         couleur         Varchar (20) NOT NULL ,
         vitesse_max     Varchar (5) NOT NULL ,
-        stock           Bool NOT NULL ,
+        prix            Int NOT NULL ,
+        stock           Int NOT NULL ,
         id_modele       Int NOT NULL ,
         id_constructeur Int NOT NULL
-	,CONSTRAINT voiture_PK PRIMARY KEY (id_voiture)
+    ,CONSTRAINT voiture_PK PRIMARY KEY (id_voiture)
 
-	,CONSTRAINT voiture_model_FK FOREIGN KEY (id_modele) REFERENCES model(id_modele)
-	,CONSTRAINT voiture_constructeur0_FK FOREIGN KEY (id_constructeur) REFERENCES constructeur(id_constructeur)
+    ,CONSTRAINT voiture_model_FK FOREIGN KEY (id_modele) REFERENCES model(id_modele)
+    ,CONSTRAINT voiture_constructeur0_FK FOREIGN KEY (id_constructeur) REFERENCES constructeur(id_constructeur)
 )ENGINE=InnoDB;
 
 
@@ -67,8 +57,10 @@ CREATE TABLE acheter(
         id_client  Int NOT NULL ,
         vendeur    Varchar (40) NOT NULL ,
         datetime   Datetime NOT NULL
-	,CONSTRAINT acheter_PK PRIMARY KEY (id_voiture,id_client)
+    ,CONSTRAINT acheter_PK PRIMARY KEY (id_voiture,id_client)
 
-	,CONSTRAINT acheter_voiture_FK FOREIGN KEY (id_voiture) REFERENCES voiture(id_voiture)
-	,CONSTRAINT acheter_client0_FK FOREIGN KEY (id_client) REFERENCES client(id_client)
+    ,CONSTRAINT acheter_voiture_FK FOREIGN KEY (id_voiture) REFERENCES voiture(id_voiture)
+    ,CONSTRAINT acheter_client0_FK FOREIGN KEY (id_client) REFERENCES client(id_client)
 )ENGINE=InnoDB;
+
+Envoyer un message à #bdd
