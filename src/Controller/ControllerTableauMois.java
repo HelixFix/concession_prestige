5f778106c9a785976baa457f9989e49a56a837c5
MIk08 <mickael.henry.mh@gmail.com>
2021-05-17 20:11:48 +0200

    derniere version mois controller

--- src/Controller/ControllerTableauMois.java
+++ src/Controller/ControllerTableauMois.java
@@ -10,13 +10,10 @@ public class ControllerTableauMois implements EventHandler<MouseEvent>  {
     private ViewHandler vh;
     private ViewTableau_Mois tbm;
 
-
-
     public ControllerTableauMois(ViewTableau_Mois tbm, ViewHandler vh) {
         this.vh = vh;
         this.tbm = tbm;
     }
-
     public void handle(MouseEvent event) {
         if (event.getSource().equals(tbm.getRetour())) {
             vh.afficherMenuPrincipal();
@@ -25,4 +22,5 @@ public class ControllerTableauMois implements EventHandler<MouseEvent>  {
         } else if (event.getSource().equals(tbm.getJour())) {
             vh.afficherTableauJour();}
     }
+
 }
